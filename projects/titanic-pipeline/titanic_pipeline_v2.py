905cb6e6f9fd8fc08c62f09a369daa1157f534c0
AadarshaMP <pandeyismyname@gmail.com>
2026-06-16 03:38:23 +0545

    pipeline_V2

---
+++ projects/titanic-pipeline/titanic_pipeline_v2.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+
+
+# LOAD DATA
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+
+#  FEATURE ENGINEERING 
+
+
+def age_group(age):
+    if age < 13:
+        return "child"
+    elif age < 20:
+        return "teen"
+    elif age < 60:
+        return "adult"
+    return "senior"
+
+df["Age"] = df["Age"].fillna(df["Age"].mean())
+
+df["AgeGroup"] = df["Age"].apply(age_group)
+df["IsRich"] = (df["Fare"] > 50).astype(int)
+df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+df["HasCabin"] = df["Cabin"].notnull().astype(int)
+df["Title"] = df["Name"].str.extract(r" ([A-Za-z]+)\.", expand=False)
+
+
+# FEATURES / TARGET
+
+features = ["Pclass", "Sex", "AgeGroup", "IsRich",
+            "FamilySize", "HasCabin", "Title"]
+
+target = "Survived"
+
+X = df[features]
+y = df[target]
+
+
+#  TRAIN TEST SPLIT (IMPORTANT)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+
+# PREPROCESSING PIPELINE
+
+
+categorical_features = ["Pclass", "Sex", "AgeGroup", "Title"]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", Pipeline([
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("encoder", OneHotEncoder(handle_unknown="ignore"))
+        ]), categorical_features),
+
+        ("num", Pipeline([
+            ("imputer", SimpleImputer(strategy="mean"))
+        ]), ["IsRich", "FamilySize", "HasCabin"])
+    ]
+)
+
+
+# MODEL PIPELINE
+
+
+model = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("classifier", RandomForestClassifier(
+        n_estimators=200,
+        random_state = 42
+    ))
+])
+
+
+#  TRAIN MODEL
+
+model.fit(X_train, y_train)
+
+rf = model.named_steps["classifier"]
+print(rf.feature_importances_)
+
+
+
+
+#  EVALUATION
+
+y_pred = model.predict(X_test)
+y_proba = model.predict_proba(X_test)[:, 1]
+
+print("\nCONFUSION MATRIX:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nCLASSIFICATION REPORT:")
+print(classification_report(y_test, y_pred))
+
+print("\nROC-AUC SCORE:")
+print(roc_auc_score(y_test, y_proba))
+
+
+# CROSS VALIDATION 
+
+cv_scores = cross_val_score(
+    model, X, y, cv=5, scoring="roc_auc"
+)
+
+print("\nCROSS VALIDATION AUC:")
+print(cv_scores)
+print("Mean AUC:", cv_scores.mean())
+print("Std AUC:", cv_scores.std())
